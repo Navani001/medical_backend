99c9401e3594d55d4f66a0b425bac31c618ecdd2
navani_hk <navaneetha2006krishnan@gmail.com>
2024-10-18 10:10:23 +0530

    ......

--- src/controllers/drug.ts
+++ src/controllers/drug.ts
@@ -1,11 +1,12 @@
-const {drugs  } = require("../../models");
+const tables = require("../../models");
 
-  export const all_drugs=async(req:any,res:any)=>{
-    
-  const data=await drugs.findAll()
-  console.log(data)
-  res.send(data)
-    
-  }
- 
+export const all_drugs = async (req: any, res: any) => {
+  const data = await tables.drugs.findAll({ include: tables.drugs_types });
   
+  res.send(data);
+};
+export const all_rx = async (req: any, res: any) => {
+  const data = await tables.rx_groups.findAll();
+ 
+  res.send(data);
+};
--- src/routes/drug.ts
+++ src/routes/drug.ts
@@ -2,6 +2,5 @@ import express from "express";
 const router = express.Router();
 const controllers = require("../controllers/drug.ts");
 router.get("/all_drugs", controllers.all_drugs);
-
+router.get("/all_rx", controllers.all_rx);
 module.exports = router;
-
