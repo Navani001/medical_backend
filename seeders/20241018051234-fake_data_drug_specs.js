46294710de73e491c7683c6e068a1b267a5b9261
navani_hk <navaneetha2006krishnan@gmail.com>
2024-10-18 11:02:28 +0530

    .........

---
+++ seeders/20241018051234-fake_data_drug_specs.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('drugs_specs', [{
+     id:1,rx_drug_id:1,no_of_days:2,
+     quantity:2,time_to_take:3,
+     comsumption_type_id:1,
+     drug_time:1,
+     comsumption_day_type_id:1, createdAt: new Date(),
+     updatedAt: new Date(),
+     
+     }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('drugs_specs', null, {});
+  }
+
+};
--- src/controllers/drug.ts
+++ src/controllers/drug.ts
@@ -12,7 +12,7 @@ export const all_rx = async (req: any, res: any) => {
 };
 export const rx_drug = async (req: any, res: any) => {
   const userId = req.params.id;
-  const data = await tables.rx_groups.findAll({include:tables.drugs});
+  const data = await tables.rx_groups.findAll({include:[{model: tables.rx_drugs, include: [{model:tables.drugs_specs,include:[tables.comsumption_types,tables.comsumption_day_types]},tables.drugs] }]});
  
   res.send(data);
 };
