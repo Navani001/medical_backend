569e5f8fd5569f448f3f8b9ca3d1ce1d7e97ac3a
navani_hk <navaneetha2006krishnan@gmail.com>
2024-10-19 22:42:08 +0530

    test for deploy

--- index.ts
+++ index.ts
@@ -3,14 +3,17 @@ const app = express();
 const { sequelize } = require("./models");
 const cors = require("cors");
 require("dotenv").config();
-const drugs=require("./src/routes/drug.ts")
+const drugs = require("./src/routes/drug.ts");
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.use("/drugs",drugs)
+app.use("/drugs", drugs);
+app.get("/", (req:any, res:any) => {
+  res.send("Hello, this is the medical backend!");
+});
 sequelize.authenticate().then(() => {
   app.listen(5000, function () {
     console.log("Server is running on port 5000");
